2ae5f4cc541dafc3160d76b8a2cd06fcebc07b51
andreas91287 <andreas_lb@me.com>
2024-10-22 19:10:04 +0200

    Complete Security on filters

--- src/main/java/ait/cohort46/security/filter/DeletePostFilter.java
+++ src/main/java/ait/cohort46/security/filter/DeletePostFilter.java
@@ -5,6 +5,7 @@ import ait.cohort46.accounting.model.Role;
 import ait.cohort46.accounting.model.UserAccount;
 import ait.cohort46.forum.dao.PostRepository;
 import ait.cohort46.forum.model.Post;
+import ait.cohort46.security.model.User;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -20,14 +21,13 @@ import java.io.IOException;
 @Order(60)
 public class DeletePostFilter implements Filter {
     private final PostRepository postRepository;
-    private final UserAccountRepository userAccountRepository;
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
         if (checkEndpoint(request.getMethod(), request.getServletPath())) {
-            UserAccount user = userAccountRepository.findById(request.getUserPrincipal().getName()).get();
+            User user = (User) request.getUserPrincipal();
             String[] parts = request.getServletPath().split("/");
             String postId = parts[parts.length - 1];
             Post post = postRepository.findById(postId).orElse(null);
@@ -35,7 +35,7 @@ public class DeletePostFilter implements Filter {
                 response.setStatus(HttpServletResponse.SC_NOT_FOUND);
                 return;
             }
-            if (!(user.getLogin().equals(post.getAuthor()) || user.getRoles().contains(Role.MODERATOR))) {
+            if (!(user.getName().equals(post.getAuthor()) || user.getRoles().contains(Role.MODERATOR.name()))) {
                 response.sendError(HttpServletResponse.SC_FORBIDDEN);
                 return;
             }
--- src/main/java/ait/cohort46/security/filter/DeleteUserFilter.java
+++ src/main/java/ait/cohort46/security/filter/DeleteUserFilter.java
@@ -1,13 +1,10 @@
 package ait.cohort46.security.filter;
 
-import ait.cohort46.accounting.dao.UserAccountRepository;
 import ait.cohort46.accounting.model.Role;
-import ait.cohort46.accounting.model.UserAccount;
 import ait.cohort46.security.model.User;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
@@ -16,19 +13,17 @@ import java.io.IOException;
 
 @Component
 @Order(40)
-@RequiredArgsConstructor
 public class DeleteUserFilter implements Filter {
-    private final UserAccountRepository repository;
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
         if (checkEndpoint(request.getMethod(), request.getServletPath())) {
-            UserAccount user = repository.findById(request.getUserPrincipal().getName()).get();
+            User user = (User) request.getUserPrincipal();
             String[] parts = request.getServletPath().split("/");
             String owner = parts[parts.length - 1];
-            if (!(user.getRoles().contains(Role.ADMINISTRATOR) || user.getLogin().equalsIgnoreCase(owner))) {
+            if (!(user.getRoles().contains(Role.ADMINISTRATOR.name()) || user.getName().equalsIgnoreCase(owner))) {
                 response.sendError(HttpServletResponse.SC_FORBIDDEN);
                 return;
             }
